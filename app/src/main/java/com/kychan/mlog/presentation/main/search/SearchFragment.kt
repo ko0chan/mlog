8ff2fb6eb6f312bd5867e6d9031074eb5939ff16
ko0chan <kychan1320@gmail.com>
2021-04-09 15:15:17 +0900

    feat(activity_main.xml) : custom view 추가

--- app/src/main/java/com/kychan/mlog/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/com/kychan/mlog/presentation/main/search/SearchFragment.kt
@@ -1,13 +1,14 @@
 package com.kychan.mlog.presentation.main.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.kychan.mlog.R
 import com.kychan.mlog.databinding.FragmentSearchBinding
+import com.kychan.mlog.presentation.main.MainActivity
 
 class SearchFragment : Fragment() {
 
@@ -22,7 +23,16 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         binding.lifecycleOwner = this
 
+        setToolbar()
+
         return binding.root
+    }
+
+    private fun setToolbar() {
+        (activity as MainActivity).supportActionBar?.run {
+            setDisplayShowCustomEnabled(true)
+            setCustomView(R.layout.view_search)
+        }
     }
 
     companion object {
