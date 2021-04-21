c0e3bb7befd35b3b655b27067d6e5d7533046409
ko0chan <kychan1320@gmail.com>
2021-04-21 17:44:22 +0900

    feat(...) : 영화검색 emptyView 추가

--- app/src/main/java/com/kychan/mlog/presentation/main/search/SearchFragment.kt
+++ app/src/main/java/com/kychan/mlog/presentation/main/search/SearchFragment.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -83,6 +84,7 @@ class SearchFragment : Fragment() {
         with(searchViewModel) {
             movieList.observe(viewLifecycleOwner, { movieList ->
                 searchMovieAdapter.submitList(movieList)
+                binding.emptyView.isVisible = movieList.isNullOrEmpty()
             })
         }
     }
