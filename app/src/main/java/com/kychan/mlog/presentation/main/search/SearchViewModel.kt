2f53b8173e9c5b685f8704c537fea9399fae37d2
ko0chan <kychan1320@gmail.com>
2021-04-23 17:00:00 +0900

    feat(...) : 검색결과와 db 비교하여 정제된 데이터 내려받기

--- app/src/main/java/com/kychan/mlog/presentation/main/search/SearchViewModel.kt
+++ app/src/main/java/com/kychan/mlog/presentation/main/search/SearchViewModel.kt
@@ -4,11 +4,14 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kychan.mlog.model.api.NaverApi
 import com.kychan.mlog.model.database.MovieDao
 import com.kychan.mlog.model.database.MovieEntity
 import com.kychan.mlog.model.response.SearchMovieResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,6 +25,18 @@ class SearchViewModel @Inject constructor(private val movieDao: MovieDao) : View
     private val _movieList = MutableLiveData<List<SearchMovieItem>>()
     val movieList: LiveData<List<SearchMovieItem>>
         get() = _movieList
+
+    private val myMovieList = MutableLiveData<List<String>>()
+
+    init {
+        viewModelScope.launch {
+            movieDao.getMovieAll().collect {
+                myMovieList.value = it.map { movieEntity ->
+                    movieEntity.toMovieItem().link
+                }
+            }
+        }
+    }
 
     fun getSearchMovie(searchKeyword: String) {
         if (searchKeyword.isEmpty()) return
@@ -37,16 +52,44 @@ class SearchViewModel @Inject constructor(private val movieDao: MovieDao) : View
                     Log.d("TAG", "성공 : ${response.raw()}")
                     Log.d("TAG", "성공 : ${response.body()?.movieResponseList}")
 
-                    _movieList.value = response.body()?.movieResponseList?.map {
-                        it.toSearchMovieItem()
-                    }
+                    _movieList.value = searchMovieItemListOf(response.body()?.movieResponseList.orEmpty(), myMovieList.value)
                 }
 
                 override fun onFailure(call: Call<SearchMovieResponse>, t: Throwable) {
                     Log.d("TAG", "실패 : $t")
                 }
-
             })
+    }
+
+    private fun searchMovieItemListOf(
+        searchMovieList: List<SearchMovieResponse.MovieResponse>,
+        myMovieLinkList: List<String>?
+    ): List<SearchMovieItem> {
+        if (myMovieLinkList.isNullOrEmpty()) {
+            return searchMovieList.map {
+                it.toSearchMovieItem()
+            }
+        }
+
+        val list = mutableListOf<SearchMovieItem>()
+        searchMovieList.forEach { movieResponse ->
+            list.add(
+                SearchMovieItem(
+                    image = movieResponse.image,
+                    title = movieResponse.title,
+                    link = movieResponse.link,
+                    subTitle = movieResponse.subTitle,
+                    director = movieResponse.director,
+                    actor = movieResponse.actor,
+                    pubDate = movieResponse.pubDate,
+                    userRating = movieResponse.userRating,
+                    isMyMovie = movieResponse.link == myMovieLinkList.find {
+                        it == movieResponse.link
+                    }
+                )
+            )
+        }
+        return list
     }
 
     fun insertMovie(item: SearchMovieItem) {
