680eafb89a6aad408d7940162b548df03739cdaf
ko0chan <kychan1320@gmail.com>
2021-04-28 21:54:28 +0900

    feat(ApiModule.kt) : api hilt 적용

---
+++ app/src/main/java/com/kychan/mlog/di/ApiModule.kt
@@ -0,0 +1,26 @@
+package com.kychan.mlog.di
+
+import com.kychan.mlog.model.api.NaverApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    private const val NAVER_URL = "https://openapi.naver.com/"
+
+    @Singleton
+    @Provides
+    fun provideNaverApi(): NaverApi {
+        return Retrofit.Builder()
+            .baseUrl(NAVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NaverApi::class.java)
+    }
+}
